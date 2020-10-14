cf9625e45061ee20cc6624c5ba59e46f8bbc5fa9
Annelies-P <annelies.pauwels@gmail.com>
2020-10-14 16:16:03 +0200

    merging changes

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,5 +1,16 @@
 export default [
   {
-    
+    match: {
+      subject: { }
+    },
+    callback: {
+      url: "http://resourcebackend/.mu/delta",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
   }
 ];
--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -3,6 +3,10 @@ defmodule Dispatcher do
 
   define_accept_types []
 
+  ###############
+  # RESOURCES
+  ###############
+
   get "/catalogs/*path", _ do
     forward conn, path, "http://cache/catalogs"
   end
@@ -15,6 +19,31 @@ defmodule Dispatcher do
     forward conn, path, "http://cache/distributions"
   end
 
+  ###############
+  # SPARQL
+  ###############
+  match "/sparql", %{ layer: :sparql, accept: %{ html: true } } do
+    forward conn, [], "http://frontend/sparql"
+  end
+
+  match "/sparql", %{ layer: :sparql, accept: %{ sparql: true } } do
+    forward conn, [], "http://database:8890/sparql"
+  end
+
+  ###############
+  # API SERVICES
+  ###############
+  get "/resource-labels/*path" do
+    Proxy.forward conn, path, "http://resource-labels/"
+  end
+
+  get "/uri-info/*path" do
+    Proxy.forward conn, path, "http://uri-info/"
+  end
+
+  #################
+  # NOT FOUND
+  #################
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
