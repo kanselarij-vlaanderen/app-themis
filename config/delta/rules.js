a1b2dc6fd4bb765b86eb08bad4f8ee6be1cb970c
Annelies-P <annelies.pauwels@gmail.com>
2020-11-10 17:13:24 +0100

    adding dcat dataset publication to stack

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,10 +1,31 @@
 export default [
   {
     match: {
-      subject: { }
+      subject: {}
     },
     callback: {
       url: "http://resourcebackend/.mu/delta",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://kanselarij.vo.data.gift/release-task-statuses/not-started'
+      }
+    },
+    callback: {
+      url: "http://dcat-dataset-publication/delta",
       method: "POST"
     },
     options: {
