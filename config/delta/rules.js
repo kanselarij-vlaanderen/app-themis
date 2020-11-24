c016b0cac8d93c242700e87df827179b3da2d4ec
Annelies-P <annelies.pauwels@gmail.com>
2020-11-24 10:19:17 +0100

    adding same-as service to stack

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -21,11 +21,32 @@ export default [
       },
       object: {
         type: 'uri',
-        value: 'http://kanselarij.vo.data.gift/release-task-statuses/not-started'
+        value: 'http://kanselarij.vo.data.gift/release-task-statuses/ready-for-release'
       }
     },
     callback: {
       url: "http://dcat-dataset-publication/delta",
+      method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://kanselarij.vo.data.gift/release-task-statuses/not-started'
+      }
+    },
+    callback: {
+      url: "http://themis-same-as/delta",
       method: "POST"
     },
     options: {
