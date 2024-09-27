b7ce89c669f0f5de70c5b3074b69e5dbd7e0d2b2
Sven Valenberghs <sven.valenberghs@faros.be>
2024-09-27 13:11:12 +0200

    fix: give active mandatees an end date and not replace all end dates

--- scripts/generate-mandatees/regeringssamenstelling.py
+++ scripts/generate-mandatees/regeringssamenstelling.py
@@ -62,11 +62,6 @@ WHERE {
 }
 ;
 
-DELETE {
-    GRAPH $graph {
-        ?mandatee mandaat:einde ?mandatee_end .
-    }
-}
 INSERT {
     GRAPH $graph {
         ?mandatee mandaat:einde $end_datetime .
@@ -77,7 +72,7 @@ WHERE {
         $gov_body a besluit:Bestuursorgaan ;
             prov:hadMember ?mandatee .
         ?mandatee a mandaat:Mandataris .
-        OPTIONAL { ?mandatee mandaat:einde ?mandatee_end . }
+        FILTER NOT EXISTS { ?mandatee mandaat:einde ?mandatee_end . }
     }
 }
 """)
