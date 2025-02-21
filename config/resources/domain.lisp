eebc1bdd3e1cb44732926dd7c5a769a158a5c4cf
Sven Valenberghs <sven.valenberghs@faros.be>
2025-02-21 14:20:29 +0100

    KAS-5010 setup for emails, no frontend access

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -14,6 +14,7 @@
 
 (read-domain-file "dcat.json")
 (read-domain-file "besluitvorming.json")
+(read-domain-file "email-domain.lisp")
 (read-domain-file "health-check.lisp")
 
 (defcall :get (base-path)
