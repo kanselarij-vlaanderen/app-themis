53f3aef61bf1bef01eaf6dcf5c691442a358f47f
Sven Valenberghs <sven.valenberghs@faros.be>
2025-03-17 16:38:39 +0100

    add lisp file for extra prefixes

---
+++ config/resources/repository.lisp
@@ -0,0 +1,6 @@
+(in-package :mu-cl-resources)
+
+(add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
+(add-prefix "nmo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#")
+(add-prefix "nie" "http://www.semanticdesktop.org/ontologies/2007/01/19/nie#")
+(add-prefix "nfo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#")
